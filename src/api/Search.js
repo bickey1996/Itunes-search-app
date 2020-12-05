eaa73e94d78586b73a27fad2c550e0e060e4da6f
bickey1996 <bickey.sah1996@gmail.com>
2020-12-05 19:56:18 +0530

    access control added

--- src/api/Search.js
+++ src/api/Search.js
@@ -2,7 +2,12 @@ import axios from "axios";
 
 export const getSearchData = async (searchtext) => {
   console.log(searchtext);
-  return axios.get("https://itunes.apple.com/search" + searchtext);
+  return axios.get("https://itunes.apple.com/search" + searchtext, {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+    },
+  });
 };
 
 export default getSearchData;
