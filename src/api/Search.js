7768a61062428e3f09e7dad3b26e30e1c5a432e8
bickey1996 <bickey.sah1996@gmail.com>
2020-12-05 20:44:33 +0530

    cors issue fixed

--- src/api/Search.js
+++ src/api/Search.js
@@ -1,13 +1,9 @@
 import axios from "axios";
+axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
 
 export const getSearchData = async (searchtext) => {
   console.log(searchtext);
-  return axios.get("https://itunes.apple.com/search" + searchtext, {
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-    },
-  });
+  return axios.get("https://itunes.apple.com/search" + searchtext);
 };
 
 export default getSearchData;
